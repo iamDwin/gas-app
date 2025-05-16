7f25a8d1f24a54991ea390a4c0d3423adb6c9e8b
Goodman Effah <godwin.goodman@pilgrimconsulting.team>
2025-05-16 01:09:45 +0000

    finish work on line chart

--- src/app/shared/components/charts/line-chart/line-chart.component.ts
+++ src/app/shared/components/charts/line-chart/line-chart.component.ts
@@ -1,88 +1,77 @@
-import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Chart, ChartConfiguration } from 'chart.js/auto';
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import ApexCharts from "apexcharts";
 
 @Component({
-  selector: 'app-line-chart',
+  selector: "app-line-chart",
   standalone: true,
-  imports: [CommonModule],
   template: `
     <div class="relative">
-      <canvas #chartCanvas></canvas>
+      <div #chartContainer></div>
     </div>
-  `
+  `,
+  imports: [],
 })
 export class LineChartComponent implements OnInit {
-  @ViewChild('chartCanvas') chartCanvas!: ElementRef;
-  @Input() data: number[] = [];
-  @Input() labels: string[] = [];
-  @Input() label: string = '';
-  @Input() borderColor: string = '#079455';
-  @Input() backgroundColor: string = 'rgba(7, 148, 85, 0.1)';
+  @ViewChild("chartContainer", { static: true }) chartContainer!: ElementRef;
 
-  private chart?: Chart;
-
-  ngOnInit() {}
-
-  ngAfterViewInit() {
-    this.createChart();
-  }
-
-  ngOnChanges() {
-    if (this.chart) {
-      this.chart.destroy();
-    }
+  ngOnInit() {
     this.createChart();
   }
 
   private createChart() {
-    if (!this.chartCanvas) return;
-
-    const ctx = this.chartCanvas.nativeElement.getContext('2d');
-    
-    const config: ChartConfiguration = {
-      type: 'line',
-      data: {
-        labels: this.labels,
-        datasets: [{
-          label: this.label,
-          data: this.data,
-          borderColor: this.borderColor,
-          backgroundColor: this.backgroundColor,
-          fill: true,
-          tension: 0.4
-        }]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: false
-          }
+    const options = {
+      chart: {
+        type: "line",
+        height: 350,
+        dropShadow: {
+          enabled: true,
+          color: "#000",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2,
         },
-        scales: {
-          y: {
-            beginAtZero: true,
-            grid: {
-              color: 'rgba(0, 0, 0, 0.1)'
-            }
-          },
-          x: {
-            grid: {
-              display: false
-            }
-          }
-        }
-      }
+        toolbar: {
+          show: false,
+        },
+      },
+      dataLabels: {
+        enabled: true,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      series: [
+        {
+          name: "Declarations",
+          data: [12, 25, 18, 30, 22, 35, 28, 40, 33, 45, 38, 50],
+          color: "#FF0000", // Red
+        },
+        {
+          name: "Nominations",
+          data: [20, 15, 25, 18, 28, 22, 32, 27, 37, 30, 40, 35],
+          color: "#079455", // Green
+        },
+      ],
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+      },
     };
 
-    this.chart = new Chart(ctx, config);
-  }
-
-  ngOnDestroy() {
-    if (this.chart) {
-      this.chart.destroy();
-    }
+    const chart = new ApexCharts(this.chartContainer.nativeElement, options);
+    chart.render();
   }
 }
